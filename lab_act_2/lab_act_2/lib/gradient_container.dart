f5659048672838f7c1041c0b0a328ea478a702d3
Daryl Mark <gabuyadaryl60@gmail.com>
2026-09-15 02:18:54 +0800

    Add parameters to GradientContainer constructor

--- lab_act_2/lab_act_2/lib/gradient_container.dart
+++ lab_act_2/lab_act_2/lib/gradient_container.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Colors.deepPurple,
-            Colors.indigo,
-          ],
+          colors: [color1, color2],
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
